007f76f108f06ad44de0ddca8a7026fd3c302698
tyzer <helder.huancara@gmail.com>
2024-09-19 08:12:53 -0500

    Conexion con ChatGPT y funcionalidad en Telegram

--- src/main/java/com/codegym/telegram/TinderBoltApp.java
+++ src/main/java/com/codegym/telegram/TinderBoltApp.java
@@ -6,8 +6,11 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class TinderBoltApp extends SimpleTelegramBot {
 
-    public static final String TELEGRAM_BOT_TOKEN = "INSERTAR TOKEN TELGRAM"; //TODO: añadir el token del bot entre comillas
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: añadir el token de ChatGPT entre comillas
+    public static final String TELEGRAM_BOT_TOKEN = "7345872590:AAGsU9Gpicz3SDAaswObY_OChJLzOHkNtqs"; //TODO: añadir el token del bot entre comillas
+    public static final String OPEN_AI_TOKEN = "gpt:yeSMIRCC7ePMWDmNqAy9JFkblB3T8MQ6g521gef0dCT5qsls"; //TODO: añadir el token de ChatGPT entre comillas
+
+    private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
+    private DialogMode mode;
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_TOKEN);
@@ -16,22 +19,51 @@ public class TinderBoltApp extends SimpleTelegramBot {
     //TODO: escribiremos la funcionalidad principal del bot aquí
 
     public void startCommand(){
+        mode = DialogMode.MAIN;
         String text = loadMessage("main");
         sendPhotoMessage("main");
         sendTextMessage(text);
+
+        showMainMenu(
+                "start", "Menu principal del bot",
+                "profile", "Generacion del perfil de Tinder \uD83D\uDE0E",
+                "opener", "Mensaje para inicializar conversacion \uD83E\uDD70",
+                "message", "Correspondencia en su nombre \uD83D\uDE08",
+                "data", "Correspondencia con celebridades \uD83D\uDD25",
+                "gpt", "Hacer una pregunta a ChatGPT \uD83E\uDDE0"
+        );
+    }
+
+    public void gptCommand(){
+        mode = DialogMode.GPT;
+        String text = loadMessage("gpt");
+        sendPhotoMessage("gpt");
+        sendTextMessage(text);
+    }
+
+    public void gptDialog(){
+        String text = getMessageText();
+        String prompt = loadPrompt("gpt");
+        String answer = chatGPT.sendMessage(prompt, text);
+        sendTextMessage(answer);
     }
 
     // respuestas de bot
     public void hello(){
-        String text = getMessageText();
-        sendTextMessage("*Hello*");
-        sendTextMessage("_How are you?_");
-        sendTextMessage("You wrote: " + text);
+        if (mode == DialogMode.GPT){
+            gptDialog();
+        }
+        else{
+            String text = getMessageText();
+            sendTextMessage("*Hello*");
+            sendTextMessage("_How are you?_");
+            sendTextMessage("You wrote: " + text);
 
-        sendPhotoMessage("avatar_main"); // enviar imagen
-        sendTextButtonsMessage("Launch Process",
-                "Start", "Start",
-                "Stop", "Stop"); // dos botones
+            sendPhotoMessage("avatar_main"); // enviar imagen
+            sendTextButtonsMessage("Launch Process",
+                    "Start", "Start",
+                    "Stop", "Stop"); // dos botones
+        }
     }
 
     // manejo de botones en telgram
@@ -50,6 +82,7 @@ public class TinderBoltApp extends SimpleTelegramBot {
     public void onInitialize() {
         //TODO: y un poco más aquí :)
         addCommandHandler("start", this::startCommand);
+        addCommandHandler("gpt", this::gptCommand);
         addMessageHandler(this::hello);
         addButtonHandler("^.*", this::helloButton);
     }
